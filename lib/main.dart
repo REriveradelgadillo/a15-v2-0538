5eb71e6fd14cc4f5109432dfd0feb7a0e45de6ba
ROBERTO_EMMANUEL_RIVERA_DELGADILLO <a.21308051280538@cbtis128.edu.mx>
2024-03-11 17:25:07 +0000

    actualizar

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:rivera0538/pantalla2_0538.dart';
+import 'package:rivera0538/pantalla1_0538.dart';
+import 'package:rivera0538/pantallaini_0538.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MiApp0538());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiApp0538 extends StatelessWidget {
+  const MiApp0538({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/",
+      routes: {
+        "/": (context) => PantallaIni_0538(),
+        "/Pantalla1_0538": (context) => Pantalla1_0538(),
+        "/Pantalla2_0538": (context) => Pantalla2_0538(),
+      }, //fin ruta paginas
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
-}
+  } //fin widget
+} //fin app
---
+++ lib/pantalla2_0538.dart
@@ -0,0 +1,22 @@
+//Pantalla2_0538
+//
+import 'package:flutter/material.dart';
+
+class Pantalla2_0538 extends StatelessWidget {
+  const Pantalla2_0538({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pantalla 2 Rivera0538'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Text(
+          'pantalla2',
+        ),
+      ),
+    );
+  }
+}
